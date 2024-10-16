377a2b2dccf17826cdd4f515fef045dc0aeaf850
sadscript <sadscripter@gmail.com>
2024-10-16 13:50:04 -0600

    fix: print all models' totals in console and show total in extension

--- query.js
+++ query.js
@@ -66,15 +66,17 @@ function searchTableRows(searchString = '') {
         console.log(`Total value of passing entries: $${totalPassValue.toFixed(2)}`);
     } else {
         console.log(`\nTotals for each unique model scraped:`);
-        let top5Models = Object.entries(groupTotals)
-            .sort((a, b) => b[1] - a[1])  // Sort in descending order
-            .slice(0, 5)  // Get top 5
-            .map(([text, total]) => ({name: text, amount: total.toFixed(2)}));
-        
-        top5Models.forEach(model => {
-            console.log(`${model.name}: $${model.amount}`);
-        });
-        
-        window.postMessage({ type: "FROM_PAGE", totalValue: totalPassValue.toFixed(2), top5Models: top5Models }, "*");
+        Object.entries(groupTotals)
+            .sort((a, b) => b[1] - a[1])
+            .forEach(([text, total]) => {
+                console.log(`${text}: $${total.toFixed(2)}`);
+            });
     }
+
+    let top5Models = Object.entries(groupTotals)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 5)
+        .map(([text, total]) => ({name: text, amount: total.toFixed(2)}));
+
+    window.postMessage({ type: "FROM_PAGE", totalValue: totalPassValue.toFixed(2), top5Models: top5Models }, "*");
 }
